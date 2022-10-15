b3200d74eb91470d8d8d4595b60a92e497e75f17
jychan99 <108382087+jychan99@users.noreply.github.com>
2022-10-15 15:17:19 +0900

    create email system

---
+++ js/email.js
@@ -0,0 +1,28 @@
+/******************************************************************************
+ * This tutorial is based on the work of Martin Hawksey twitter.com/mhawksey  *
+ * But has been simplified and cleaned up to make it more beginner friendly   *
+ * All credit still goes to Martin and any issues/complaints/questions to me. *
+ ******************************************************************************/
+
+var TO_ADDRESS = "star2121513@naver.com"; // where to send form data
+
+function doPost(e) {
+  try {
+    Logger.log(e); // the Google Script version of console.log see: Class Logger
+    MailApp.sendEmail(
+      TO_ADDRESS,
+      "Message from your portfolio",
+      JSON.stringify(e.parameters)
+    );
+    // return json success results
+    return ContentService.createTextOutput(
+      JSON.stringify({ result: "success", data: JSON.stringify(e.parameters) })
+    ).setMimeType(ContentService.MimeType.JSON);
+  } catch (error) {
+    // if error return this
+    Logger.log(error);
+    return ContentService.createTextOutput(
+      JSON.stringify({ result: "error", error: e })
+    ).setMimeType(ContentService.MimeType.JSON);
+  }
+}
